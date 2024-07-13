84c8f989224bc048c6d967fc60e7e0e9852dd781
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-07-13 10:34:08 -0300

    raw: 'creating and mapping entity Product'

---
+++ src/main/java/com/zex/ecommerce/models/product/Product.java
@@ -0,0 +1,46 @@
+package com.zex.ecommerce.models.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity(name = "product")
+@Table(name = "produtcs")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "product_name")
+    private String name;
+
+
+    @Column(name = "product_description")
+    private String description;
+
+
+    @Column(name = "product_value")
+    private BigDecimal value;
+
+
+    @Column(name = "product_stock")
+    private Integer stock;
+
+
+    @Column(name = "product_category")
+    @Enumerated(value = EnumType.STRING)
+    private Category category;
+
+
+    @Column(name = "product_register_date")
+    private LocalDate registerDate;
+}
