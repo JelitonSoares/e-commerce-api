179ed97ea559c69640cb63d3b0ffc766fddd25d2
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-07-13 10:34:28 -0300

    raw: 'creating enum Category'

---
+++ src/main/java/com/zex/ecommerce/models/product/Category.java
@@ -0,0 +1,14 @@
+package com.zex.ecommerce.models.product;
+
+public enum Category {
+    LIMPEZA,
+    HIGINENE,
+    ALIMENTICIOS,
+    PADARIA,
+    ACOUGUE,
+    BEBIDAS,
+    LATICINIOS,
+    UTILIDADES,
+    HORTFRUT,
+    DOCES
+}
