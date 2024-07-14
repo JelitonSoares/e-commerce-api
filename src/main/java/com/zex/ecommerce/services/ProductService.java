292961730013552b8191e1afaefb20e5c6c0078b
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-07-14 10:25:12 -0300

    feat: 'implements update method to update a product'

--- src/main/java/com/zex/ecommerce/services/ProductService.java
+++ src/main/java/com/zex/ecommerce/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.zex.ecommerce.services;
 
 import com.zex.ecommerce.dtos.ProductDTO;
+import com.zex.ecommerce.dtos.UpdateProductDTO;
 import com.zex.ecommerce.models.product.Product;
 import com.zex.ecommerce.repositories.ProductRepository;
 import jakarta.transaction.Transactional;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,4 +34,20 @@ public class ProductService {
                 .map(p -> new ProductDTO(p))
                 .collect(Collectors.toList());
     }
+
+    @Transactional
+    public ProductDTO update(UpdateProductDTO data) {
+        Product product = this.repository.getReferenceById(data.id());
+
+        product.updateProduct(data);
+
+        return new ProductDTO(product);
+    }
+
+//    @Transactional
+//    public void delete(UUID id) {
+//        Product product = this.repository.getReferenceById(id);
+//
+//        this.repository.delete(product);
+//    }
 }
