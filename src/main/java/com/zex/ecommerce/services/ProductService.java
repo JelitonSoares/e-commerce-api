5bcb5689ba0913c23ed56b65f096fa269cc0f05c
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-07-14 08:49:49 -0300

    feat: 'implements a save method to persist a Product'

--- src/main/java/com/zex/ecommerce/services/ProductService.java
+++ src/main/java/com/zex/ecommerce/services/ProductService.java
@@ -1,4 +1,24 @@
 package com.zex.ecommerce.services;
 
+import com.zex.ecommerce.dtos.ProductDTO;
+import com.zex.ecommerce.models.product.Product;
+import com.zex.ecommerce.repositories.ProductRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
 public class ProductService {
+    @Autowired
+    private ProductRepository repository;
+
+    @Transactional
+    public ProductDTO save(ProductDTO data) {
+        Product product = new Product(data);
+        product.setRegisterDate(LocalDate.now());
+
+        return new ProductDTO(this.repository.save(product));
+    }
 }
