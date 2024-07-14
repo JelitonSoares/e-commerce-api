a12530885bd449fb2ec557fa827fe405b568ccc4
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-07-14 09:40:07 -0300

    feat: 'implements save method and mapping GET endpoint on ProductController'

---
+++ src/main/java/com/zex/ecommerce/controllers/ProductController.java
@@ -0,0 +1,24 @@
+package com.zex.ecommerce.controllers;
+
+import com.zex.ecommerce.dtos.ProductDTO;
+import com.zex.ecommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ecommerce/products")
+public class ProductController {
+    @Autowired
+    private ProductService service;
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> save(@RequestBody ProductDTO data) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(data));
+    }
+
+}
