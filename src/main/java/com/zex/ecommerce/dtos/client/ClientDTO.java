4e4bb111539352ad1ec786500d7def75945d57dd
JelitonSoares <jelitonn.soarres@gmail.com>
2024-07-15 21:30:47 -0300

    feat: 'implements constructor method that receive a Client'

--- src/main/java/com/zex/ecommerce/dtos/client/ClientDTO.java
+++ src/main/java/com/zex/ecommerce/dtos/client/ClientDTO.java
@@ -1,9 +1,15 @@
 package com.zex.ecommerce.dtos.client;
 
+import com.zex.ecommerce.models.client.Client;
+
 import java.util.UUID;
 
 public record ClientDTO(UUID id,
                         String name,
                         String document,
                         AddressDTO address) {
+
+    public ClientDTO(Client client) {
+        this(client.getId(), client.getName(), client.getDocument(), new AddressDTO(client.getAddress()));
+    }
 }
