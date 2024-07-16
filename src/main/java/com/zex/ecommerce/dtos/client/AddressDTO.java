30e1f296d1b591d03c53d96664c37e768fb73a96
JelitonSoares <jelitonn.soarres@gmail.com>
2024-07-15 21:31:15 -0300

    feat: 'implements constructor method that receive an Address'

--- src/main/java/com/zex/ecommerce/dtos/client/AddressDTO.java
+++ src/main/java/com/zex/ecommerce/dtos/client/AddressDTO.java
@@ -1,5 +1,7 @@
 package com.zex.ecommerce.dtos.client;
 
+import com.zex.ecommerce.models.client.Address;
+
 public record AddressDTO(String publicPlace,
                          Integer number,
                          String neighborhood,
@@ -7,4 +9,9 @@ public record AddressDTO(String publicPlace,
                          String uf,
                          String cep,
                          String complement) {
+
+    public AddressDTO(Address address) {
+        this(address.getPublicPlace(), address.getNumber(), address.getNeighborhood(), address.getCity(), address.getUf(),
+                address.getCep(), address.getComplement());
+    }
 }
