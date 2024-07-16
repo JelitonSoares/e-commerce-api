693e5ab59606e4788271e84c0fa24529193b01b3
JelitonSoares <jelitonn.soarres@gmail.com>
2024-07-15 21:44:56 -0300

    feat: 'add validations'

--- src/main/java/com/zex/ecommerce/dtos/client/AddressDTO.java
+++ src/main/java/com/zex/ecommerce/dtos/client/AddressDTO.java
@@ -1,12 +1,20 @@
 package com.zex.ecommerce.dtos.client;
 
 import com.zex.ecommerce.models.client.Address;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
-public record AddressDTO(String publicPlace,
+public record AddressDTO(@NotBlank String publicPlace,
                          Integer number,
+                         @NotBlank
                          String neighborhood,
+                         @NotBlank
                          String city,
+                         @NotBlank
                          String uf,
+                         @NotBlank
+                                 @Pattern(regexp = "\\d{8}")
                          String cep,
                          String complement) {
 
--- src/main/java/com/zex/ecommerce/dtos/client/ClientDTO.java
+++ src/main/java/com/zex/ecommerce/dtos/client/ClientDTO.java
@@ -1,12 +1,21 @@
 package com.zex.ecommerce.dtos.client;
 
 import com.zex.ecommerce.models.client.Client;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.util.UUID;
 
 public record ClientDTO(UUID id,
+                        @NotBlank
                         String name,
+                        @NotBlank
+                        @Pattern(regexp = "\\d{11}")
                         String document,
+                        @NotNull
+                        @Valid
                         AddressDTO address) {
 
     public ClientDTO(Client client) {
