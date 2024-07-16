4518b1921f1c848cef46faa4d815ecc172790ace
JelitonSoares <jelitonn.soarres@gmail.com>
2024-07-15 22:07:25 -0300

    feat: 'implements findAll method and mapping Get endpoint'

--- src/main/java/com/zex/ecommerce/controllers/ClientController.java
+++ src/main/java/com/zex/ecommerce/controllers/ClientController.java
@@ -6,10 +6,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ecommerce/clients")
@@ -21,5 +20,11 @@ public class ClientController {
     @PostMapping
     public ResponseEntity<ClientDTO> save(@RequestBody @Valid ClientDTO data) {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(data));
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<ClientDTO>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(this.service.findAll());
     }
 }
