af0fc1d40b00d1bd039773f2b1e76278a8972b11
JelitonSoares <jelitonn.soarres@gmail.com>
2024-07-15 21:31:44 -0300

    feat: 'create save method to persist a Client'

--- src/main/java/com/zex/ecommerce/services/ClientService.java
+++ src/main/java/com/zex/ecommerce/services/ClientService.java
@@ -1,4 +1,23 @@
 package com.zex.ecommerce.services;
 
+import com.zex.ecommerce.dtos.client.ClientDTO;
+import com.zex.ecommerce.models.client.Client;
+import com.zex.ecommerce.repositories.ClientRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClientService {
+
+
+    @Autowired
+    private ClientRepository repository;
+
+    @Transactional
+    public ClientDTO save(ClientDTO data) {
+        Client client = new Client(data);
+
+        return new ClientDTO(this.repository.save(client));
+    }
 }
