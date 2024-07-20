1f3f5436e2e513458bcf05b46ed5e049dec7b6e6
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-07-20 12:42:41 -0300

    feat: 'map endpoint Get'

--- src/main/java/com/zex/ecommerce/controllers/OrderController.java
+++ src/main/java/com/zex/ecommerce/controllers/OrderController.java
@@ -6,10 +6,10 @@ import com.zex.ecommerce.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Scanner;
 
 @RestController
 @RequestMapping("/ecommerce/orders")
@@ -20,5 +20,10 @@ public class OrderController {
     @PostMapping
     public ResponseEntity<OrderDTO> create(@RequestBody CreateOrderDTO data) {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.service.create(data));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderDTO>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(this.service.findAll());
     }
 }
