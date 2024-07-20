392517ee4d46ec48f6268a5b55cc9c3bcb210513
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-07-20 11:48:53 -0300

    feat: 'implements constructor method that receive a corresponding entity'

--- src/main/java/com/zex/ecommerce/dtos/order/OrderDTO.java
+++ src/main/java/com/zex/ecommerce/dtos/order/OrderDTO.java
@@ -1,9 +1,17 @@
 package com.zex.ecommerce.dtos.order;
 
+import com.zex.ecommerce.models.order.Order;
+
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public record OrderDTO(UUID id,
                        UUID clientId,
                        List<OrderedProductsDTO> products) {
+
+    public OrderDTO(Order data) {
+        this(data.getId(), data.getClient().getId(),  data.getOrderedItensList().stream()
+                .map(i -> new OrderedProductsDTO(i)).collect(Collectors.toList()));
+    }
 }
--- src/main/java/com/zex/ecommerce/dtos/order/OrderedProductsDTO.java
+++ src/main/java/com/zex/ecommerce/dtos/order/OrderedProductsDTO.java
@@ -1,7 +1,14 @@
 package com.zex.ecommerce.dtos.order;
 
+import com.zex.ecommerce.models.ordereditens.OrderedItens;
+
+import java.util.List;
 import java.util.UUID;
 
 public record OrderedProductsDTO(UUID productId,
                                  Integer amount) {
+
+    public OrderedProductsDTO(OrderedItens data) {
+        this(data.getId(), data.getAmount());
+    }
 }
