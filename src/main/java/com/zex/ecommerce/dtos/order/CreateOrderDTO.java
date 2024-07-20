b1dcb3cb1581f9ae6d064f3aa5e7ca5dbd8750d6
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-07-20 12:24:26 -0300

    feat: 'rename to CreateOrderDTO'

---
+++ src/main/java/com/zex/ecommerce/dtos/order/CreateOrderDTO.java
@@ -0,0 +1,17 @@
+package com.zex.ecommerce.dtos.order;
+
+import com.zex.ecommerce.models.order.Order;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public record CreateOrderDTO(UUID id,
+                             UUID clientId,
+                             List<CreateOrderProductDTO> products) {
+
+    public CreateOrderDTO(Order data) {
+        this(data.getId(), data.getClient().getId(),  data.getOrderedItensList().stream()
+                .map(i -> new CreateOrderProductDTO(i)).collect(Collectors.toList()));
+    }
+}
