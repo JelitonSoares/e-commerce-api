86ce034c5df01fdb7c9c21943311b8b1d21d08b2
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-07-21 12:09:06 -0300

    feat: 'change the return from create method applying the location field on response'

--- src/main/java/com/zex/ecommerce/controllers/ClientController.java
+++ src/main/java/com/zex/ecommerce/controllers/ClientController.java
@@ -1,14 +1,18 @@
 package com.zex.ecommerce.controllers;
 
 import com.zex.ecommerce.dtos.client.CreateClientDTO;
+import com.zex.ecommerce.dtos.client.DetailsClientDTO;
 import com.zex.ecommerce.dtos.client.UpdateClientDTO;
+import com.zex.ecommerce.models.client.Client;
 import com.zex.ecommerce.services.ClientService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,8 +24,13 @@ public class ClientController {
     private ClientService service;
 
     @PostMapping
-    public ResponseEntity<CreateClientDTO> save(@RequestBody @Valid CreateClientDTO data) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(data));
+    public ResponseEntity<DetailsClientDTO> create(@RequestBody @Valid CreateClientDTO data, UriComponentsBuilder builder) {
+        DetailsClientDTO client = this.service.create(data);
+
+
+        URI uri = builder.path("/ecommerce/clients/{id}").buildAndExpand(client.id()).toUri();
+
+        return ResponseEntity.status(HttpStatus.CREATED).location(uri).body(client);
     }
 
 
