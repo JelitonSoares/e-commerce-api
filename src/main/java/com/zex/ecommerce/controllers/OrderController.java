188d2f11463e9c6a6c4493e330c39e8a0f193adf
JelitonSoares <jelitonn.soarres@gmail.com>
2024-09-05 20:13:48 -0300

    feat: 'change a findAll method apply good practices'

--- src/main/java/com/zex/ecommerce/controllers/OrderController.java
+++ src/main/java/com/zex/ecommerce/controllers/OrderController.java
@@ -1,16 +1,21 @@
 package com.zex.ecommerce.controllers;
 
-import com.zex.ecommerce.domain.order.OrderDTO;
 import com.zex.ecommerce.domain.order.CreateOrderDTO;
+import com.zex.ecommerce.domain.order.OrderDTO;
+import com.zex.ecommerce.domain.order.OrderSimplifiedDTO;
 import com.zex.ecommerce.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("/ecommerce/orders")
@@ -30,7 +35,7 @@ public class OrderController {
     }
 
     @GetMapping
-    public ResponseEntity<List<OrderDTO>> findAll() {
-        return ResponseEntity.status(HttpStatus.OK).body(this.service.findAll());
+    public ResponseEntity<PagedModel<OrderSimplifiedDTO>> findAll(@PageableDefault(size = 10, sort = {"orderDate"}, direction = Sort.Direction.DESC ) Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(new PagedModel<OrderSimplifiedDTO>(this.service.findAll(pageable)));
     }
 }
