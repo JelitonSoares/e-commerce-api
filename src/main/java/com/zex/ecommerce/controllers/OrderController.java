7a35d146ec2db2e33cff667802b9de0dd95e84f2
JelitonSoares <jelitonn.soarres@gmail.com>
2024-09-05 19:45:03 -0300

    feat: 'factor create method to returns location field'

--- src/main/java/com/zex/ecommerce/controllers/OrderController.java
+++ src/main/java/com/zex/ecommerce/controllers/OrderController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,8 +19,14 @@ public class OrderController {
     private OrderService service;
 
     @PostMapping
-    public ResponseEntity<OrderDTO> create(@RequestBody CreateOrderDTO data) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(this.service.create(data));
+    public ResponseEntity<OrderDTO> create(@RequestBody CreateOrderDTO data, UriComponentsBuilder builder) {
+
+        OrderDTO newOrder = this.service.create(data);
+
+        URI uri = builder.path("/ecommerce/orders/{id}").buildAndExpand(newOrder.id()).toUri();
+
+
+        return ResponseEntity.status(HttpStatus.CREATED).location(uri).body(newOrder);
     }
 
     @GetMapping
