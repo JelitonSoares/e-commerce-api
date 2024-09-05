af4f571bb7f4084d6c7fe1f890be8317ced4a618
JelitonSoares <jelitonn.soarres@gmail.com>
2024-09-05 20:16:59 -0300

    feat: 'remove unusable imports'

--- src/main/java/com/zex/ecommerce/controllers/OrderController.java
+++ src/main/java/com/zex/ecommerce/controllers/OrderController.java
@@ -5,7 +5,6 @@ import com.zex.ecommerce.domain.order.OrderDTO;
 import com.zex.ecommerce.domain.order.OrderSimplifiedDTO;
 import com.zex.ecommerce.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
