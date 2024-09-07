0d0fdd8f58c058a0f5da211881cbcd082199f042
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-09-07 11:26:36 -0300

    feat: 'add validations on UpdateOrderDTO'

--- src/main/java/com/zex/ecommerce/domain/order/UpdateOrderDTO.java
+++ src/main/java/com/zex/ecommerce/domain/order/UpdateOrderDTO.java
@@ -1,8 +1,12 @@
 package com.zex.ecommerce.domain.order;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
-public record UpdateOrderDTO(UUID orderId,
+public record UpdateOrderDTO(@NotNull UUID orderId,
+                             @NotNull
                              UUID productId,
+                             @NotNull
                              Integer amount) {
 }
