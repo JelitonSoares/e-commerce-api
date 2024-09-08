ab495757df7edb0c4e24d6af415b7d9729a904e2
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-09-08 08:54:49 -0300

    feat: 'implements some methods to catch exceptions'

--- src/main/java/com/zex/ecommerce/infra/exceptions/RestExceptionHandler.java
+++ src/main/java/com/zex/ecommerce/infra/exceptions/RestExceptionHandler.java
@@ -1,7 +1,47 @@
 package com.zex.ecommerce.infra.exceptions;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionMessage> exceptionHandler(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ExceptionMessage(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ValidationMessageError>> methodArgumentNotValidHandler(MethodArgumentNotValidException exception) {
+        List<FieldError> fieldErrors = exception.getFieldErrors();
+
+        List<ValidationMessageError> errors = fieldErrors.stream()
+                    .map(fieldError -> new ValidationMessageError(fieldError))
+                    .collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ExceptionMessage> httpMessageNotReadableHandler(HttpMessageNotReadableException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionMessage(exception.getMessage(), HttpStatus.BAD_REQUEST));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ExceptionMessage> entityNotFoundHandler(EntityNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionMessage(exception.getMessage(), HttpStatus.NOT_FOUND));
+    }
+
+
 }
