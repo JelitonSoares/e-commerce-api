27977849a07e17496310df08c003101c5d28bc03
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-11-09 11:47:42 -0300

    feat: 'create query to find product by name'

--- src/main/java/com/zex/ecommerce/repositories/ProductRepository.java
+++ src/main/java/com/zex/ecommerce/repositories/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.zex.ecommerce.repositories;
 
 import com.zex.ecommerce.domain.product.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
+
 
 public interface ProductRepository extends JpaRepository<Product, String> {
+
+
+    Page<Product> findByNameContainingIgnoreCaseOrderByValue(String name, Pageable pageable);
 }
