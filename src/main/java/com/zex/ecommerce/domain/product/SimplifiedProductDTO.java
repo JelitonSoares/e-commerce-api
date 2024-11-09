23e761ca044f9bfa85d59290b44adabf57453334
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-11-09 11:50:03 -0300

    feat: 'includes field value'

--- src/main/java/com/zex/ecommerce/domain/product/SimplifiedProductDTO.java
+++ src/main/java/com/zex/ecommerce/domain/product/SimplifiedProductDTO.java
@@ -1,12 +1,14 @@
 package com.zex.ecommerce.domain.product;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public record SimplifiedProductDTO(String id,
                                    String name,
-                                   String description) {
+                                   String description,
+                                   BigDecimal value) {
 
     public SimplifiedProductDTO(Product product) {
-        this(product.getId(), product.getName(), product.getDescription());
+        this(product.getId(), product.getName(), product.getDescription(), product.getValue());
     }
 }
