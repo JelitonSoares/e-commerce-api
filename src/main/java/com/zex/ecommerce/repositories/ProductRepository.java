a21bcc656e04a8f7b2bc9977e5a65092fce4a07e
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-11-09 12:13:03 -0300

    feat: 'creates find by category method'

--- src/main/java/com/zex/ecommerce/repositories/ProductRepository.java
+++ src/main/java/com/zex/ecommerce/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.zex.ecommerce.repositories;
 
+import com.zex.ecommerce.domain.product.Category;
 import com.zex.ecommerce.domain.product.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,4 +12,6 @@ public interface ProductRepository extends JpaRepository<Product, String> {
 
 
     Page<Product> findByNameContainingIgnoreCaseOrderByValue(String name, Pageable pageable);
+
+    Page<Product> findByCategoryOrderByValue(Category category, Pageable pageable);
 }
