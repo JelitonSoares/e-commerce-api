12dd2c1e1482724e79f2b9420a5f32ea6e896f69
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-11-10 09:56:47 -0300

    feat: 'create findByName query method'

--- src/main/java/com/zex/ecommerce/repositories/ClientRepository.java
+++ src/main/java/com/zex/ecommerce/repositories/ClientRepository.java
@@ -1,9 +1,14 @@
 package com.zex.ecommerce.repositories;
 
 import com.zex.ecommerce.domain.client.Client;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface ClientRepository extends JpaRepository<Client, String> {
+
+
+    Page<Client> findByName(String name, Pageable pageable);
 }
