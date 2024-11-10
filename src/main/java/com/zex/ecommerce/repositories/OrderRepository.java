e046cf0fea530c43428a93f096039ce1c0887c75
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-11-10 10:32:49 -0300

    feat: 'create method query to find a order by client'

--- src/main/java/com/zex/ecommerce/repositories/OrderRepository.java
+++ src/main/java/com/zex/ecommerce/repositories/OrderRepository.java
@@ -1,9 +1,15 @@
 package com.zex.ecommerce.repositories;
 
+import com.zex.ecommerce.domain.client.Client;
 import com.zex.ecommerce.domain.order.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, String> {
+
+
+    Page<Order> findByClientOrderByOrderDateDesc(Client client, Pageable pageable);
 }
