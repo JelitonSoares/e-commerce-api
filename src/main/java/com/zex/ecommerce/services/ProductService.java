94a28d6fd36e7b936fec5e144d77bf516a7a2961
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-11-14 14:08:29 -0300

    feat: 'creates getBestSellingProduct method'

--- src/main/java/com/zex/ecommerce/services/ProductService.java
+++ src/main/java/com/zex/ecommerce/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.zex.ecommerce.services;
 
 import com.zex.ecommerce.domain.product.*;
+import com.zex.ecommerce.repositories.OrderedItemRepository;
 import com.zex.ecommerce.repositories.ProductRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,31 +11,32 @@ import org.springframework.stereotype.Service;
 
 
 import java.time.LocalDate;
-import java.util.UUID;
 
 @Service
 public class ProductService {
     @Autowired
-    private ProductRepository repository;
+    private ProductRepository productRepository;
+    @Autowired
+    private OrderedItemRepository orderedItemRepository;
 
     @Transactional
     public DetailsProductDTO create(CreateProductDTO data) {
         Product product = new Product(data);
         product.setRegisterDate(LocalDate.now());
 
-        return new DetailsProductDTO(this.repository.save(product));
+        return new DetailsProductDTO(this.productRepository.save(product));
     }
 
 
     public Page<SimplifiedProductDTO> getAll(Pageable pageable) {
-        Page<Product> products = this.repository.findAll(pageable);
+        Page<Product> products = this.productRepository.findAll(pageable);
 
         return products.map(p -> new SimplifiedProductDTO(p));
     }
 
     @Transactional
     public DetailsProductDTO update(UpdateProductDTO data) {
-        Product product = this.repository.getReferenceById(data.id());
+        Product product = this.productRepository.getReferenceById(data.id());
 
         product.updateProduct(data);
 
@@ -43,9 +45,9 @@ public class ProductService {
 
     @Transactional
     public void delete(String id) {
-        Product product = this.repository.getReferenceById(id);
+        Product product = this.productRepository.getReferenceById(id);
 
-        this.repository.delete(product);
+        this.productRepository.delete(product);
     }
 
 
@@ -56,20 +58,26 @@ public class ProductService {
     }
 
     public Product getReferenceByID(String id) {
-        return this.repository.getReferenceById(id);
+        return this.productRepository.getReferenceById(id);
     }
 
 
     public Page<SimplifiedProductDTO> findByName(String name, Pageable pageable) {
 
 
-        return this.repository.findByNameContainingIgnoreCaseOrderByValue(name, pageable)
+        return this.productRepository.findByNameContainingIgnoreCaseOrderByValue(name, pageable)
                 .map(p -> new SimplifiedProductDTO(p));
     }
 
 
     public Page<SimplifiedProductDTO> findByCategory(Category category, Pageable pageable) {
-        return this.repository.findByCategoryOrderByValue(category, pageable)
+        return this.productRepository.findByCategoryOrderByValue(category, pageable)
                 .map(p -> new SimplifiedProductDTO(p));
     }
+
+
+    public Page<BestSellingProduct> getBestSellingProducts(Pageable pageable) {
+        return this.orderedItemRepository.findBestSellingProducts(pageable);
+    }
+
 }
