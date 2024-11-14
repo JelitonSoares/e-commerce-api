a17c3aeba1b2503d4e204659b5f295fc3c933490
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-11-14 16:28:15 -0300

    feat: 'create TotalAmountSpentByClients'

---
+++ src/main/java/com/zex/ecommerce/domain/client/query/TotalAmountSpentByClients.java
@@ -0,0 +1,9 @@
+package com.zex.ecommerce.domain.client.query;
+
+import java.math.BigDecimal;
+
+public interface TotalAmountSpentByClients {
+
+    String getClientName();
+    BigDecimal getTotalAmountSpent();
+}
